2bc2cd32ba8b7cac2e13f7667f3dd79b755982f9
fekadusisay21 <fekadusisay07@gmail.com>
2024-09-04 10:08:59 +0300

    initial commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineEmits } from "vue";
+
+const emits = defineEmits(["select-option"]);
+
+const selectOption = (isColor) => {
+  emits("select-option", isColor);
+};
+</script>
+
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
+        title="Jane Doe"
+        subtitle="jane.doe@example.com"
+      ></v-list-item>
+    </v-list>
+
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectOption(true)"
+      >
+      </v-list-item>
+      <v-list-item
+        prepend-icon="mdi-dialpad"
+        title="Grayspace Gallery"
+        value="color"
+        @click="selectOption(false)"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
